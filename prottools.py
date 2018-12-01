b00caafc942290c96c1545b5a2514e13405c3cc5
icogg <45195685+icogg@users.noreply.github.com>
2018-12-01 17:15:24 +1000

    Update prottools.py

--- prottools.py
+++ prottools.py
@@ -1,17 +1,5 @@
 """Module Docstring"""
 
-def vanwarrington(Ifault, length=1):
-    """Calculates the resistance of an arc given a fault current and length
-Inputs: 
-    Ifault = Fault current in Amperes
-    length = are length in meters
-
-Output:
-    Resistance in ohms to two decimal places"""
-    
-    Resistance = round(28710 * length / Ifault**1.4, 2)
-    return Resistance
-
 def testmodule(n):
     n += 1
     return n
