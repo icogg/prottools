930009945f7cd12cb9414d7d956ed3855ae85585
icogg <45195685+icogg@users.noreply.github.com>
2018-12-02 16:31:54 +1000

    Update __init__.py

--- p543/__init__.py
+++ p543/__init__.py
@@ -3,22 +3,3 @@ import math
 from .currenttransformer import ct_perf
 from .linediff import diff
 from .linedetails import line_details
-
-			
-def _CT(CT=[]):
-	"""Input a list containing [CTPrimary, CTSecondary]. The return is a 
-	 dictionary with primary, secondary and ratio"""
-	if len(CT) == 2:
-		CTPrim = max(CT)
-		CTSec = min(CT)
-		CTRatio = CTPrim / CTSec
-	else:
-		return print('CT Ratio required')
-	return{'CTPrim':CTPrim
-	       , 'CTSec':CTSec
-	       , 'CTRatio':CTRatio
-	      }
-
-def _myround(x, base=5):
-	return base * round(float(x) / base)
-	
