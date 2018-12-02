3632f785b201a0726330c8a7db212227c41f79ef
icogg <45195685+icogg@users.noreply.github.com>
2018-12-02 15:52:22 +1000

    Create vanwarrington.py

---
+++ vanwarrington.py
@@ -0,0 +1,11 @@
+def vanwarrington(Ifault, length=1):
+    """Calculates the resistance of an arc given a fault current and length
+Inputs: 
+    Ifault = Fault current in Amperes
+    length = are length in meters
+
+Output:
+    Resistance in ohms to two decimal places"""
+    
+    Resistance = round(28710 * length / Ifault**1.4, 2)
+    return Resistance
