a58179168cc518a39beb372cf727e51d0223d08c
icogg <45195685+icogg@users.noreply.github.com>
2018-12-02 15:51:46 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,3 @@
-def vanwarrington(Ifault, length=1):
-    """Calculates the resistance of an arc given a fault current and length
-Inputs: 
-    Ifault = Fault current in Amperes
-    length = are length in meters
-
-Output:
-    Resistance in ohms to two decimal places"""
-    
-    Resistance = round(28710 * length / Ifault**1.4, 2)
-    return Resistance
-
 
 def apparentimpedance(polar=True
                   , Vrelay = []
