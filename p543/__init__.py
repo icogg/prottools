b7af31266ff9634a5d4f56aa06a65bdb5f11962b
icogg <45195685+icogg@users.noreply.github.com>
2018-12-02 17:14:38 +1000

    Update __init__.py

--- p543/__init__.py
+++ p543/__init__.py
@@ -1,6 +1,6 @@
 # import cmath
 # import math
 # from .currenttransformer import ct_perf
-import ct_perf
+from .currenttransformer import ct_perf
 from .linediff import diff
 # from .linedetails import line_details
