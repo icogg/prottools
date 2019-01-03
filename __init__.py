c4aca8fd11daf41b31c29ff97d7aea3983e6000a
icogg <45195685+icogg@users.noreply.github.com>
2019-01-03 15:56:04 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
 from .vanwarrington import *
 from .apparentimpedance import *
 from .capbank import *
+
+
+class PDF(object):
+  def __init__(self, pdf, size=(900,600)):
+    self.pdf = pdf.replace(' ', '%20')
+    self.size = size
+
+  def _repr_html_(self):
+    return '<iframe src={0} width={1[0]} height={1[1]}></iframe>'.format(self.pdf, self.size)
+
+  def _repr_latex_(self):
+    return r'\includegraphics[width=1.0\textwidth]{{{0}}}'.format(self.pdf)
